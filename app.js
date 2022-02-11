81d0d68a71ad051f55bee8d3f69eb0f44057f2fe
davidlippai <lippai28@gmail.com>
2022-02-11 19:32:02 +0100

    ready

--- app.js
+++ app.js
@@ -2,9 +2,20 @@
 
 let changerBtn = document.getElementById("changerBtn");
 
-let colorArray = ["red", "green", "blue", "yellow"];
+let colorArray = ["Red", "Green", "Blue", "Yellow"];
+
+
 
 changerBtn.addEventListener('click',function(){
   let randomColor = colorArray[Math.floor(Math.random()*colorArray.length)]
   document.body.style.backgroundColor = randomColor;
+
+  document.getElementById("colorName").innerHTML = " " + randomColor;
+
+  if (randomColor === "Red" || randomColor === "Blue") {
+    document.getElementById("name").style.color = "white";
+  } else {
+    document.getElementById("name").style.color = "black";
+  }
+
 })
